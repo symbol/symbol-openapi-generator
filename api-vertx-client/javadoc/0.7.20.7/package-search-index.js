f4274a50441f44c348d9fbd5a364d14dac76ee20
Fernando Boucquez <fboucquez@gmail.com>
2020-01-16 11:28:22 -0300

    Publishing api-vertx-client Generated Client Javadocs for version 0.7.20.7

---
+++ api-vertx-client/javadoc/0.7.20.7/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.nem.sdk.openapi.vertx.api"},{"l":"io.nem.sdk.openapi.vertx.api.rxjava"},{"l":"io.nem.sdk.openapi.vertx.invoker"},{"l":"io.nem.sdk.openapi.vertx.invoker.auth"},{"l":"io.nem.sdk.openapi.vertx.model"}]
