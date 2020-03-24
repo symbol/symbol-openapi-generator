fd260419f9f5eb982d2614b56129163ab427b2f6
Fernando Boucquez <fboucquez@gmail.com>
2020-03-23 23:07:51 -0300

    Improved validation and documentation

--- download-and-patch.sh
+++ download-and-patch.sh
@@ -5,9 +5,20 @@ SNAPSHOT_PREFIX=-SNAPSHOT
 FULL_VERSION=$(head -n 1 version.txt)
 VERSION="${FULL_VERSION%$SNAPSHOT_PREFIX}"
 
-wget -q "https://github.com/nemtech/symbol-openapi/releases/download/v$VERSION/openapi3.yml" -O openapi3.yml
+rm -f openapi3.yml
+rm -f openapi3-any-of-patch.yml
+wget "https://github.com/nemtech/symbol-openapi/releases/download/v$VERSION/openapi3.yml" -O openapi3.yml
 
 cp openapi3.yml openapi3-any-of-patch.yml
+
+OPEN_API_VERSION="$(yaml get openapi3-any-of-patch.yml info.version)"
+
+if [ "$OPEN_API_VERSION" != "$VERSION" ]; then
+    echo "Download open api version $OPEN_API_VERSION doesn't not match current version $VERSION !!!!"
+    exit 1
+fi
+
+echo "Open api version $OPEN_API_VERSION has been downloaded"
 
 for value in EmbeddedTransactionInfoDTO.properties.transaction \
               TransactionInfoDTO.properties.transaction \
